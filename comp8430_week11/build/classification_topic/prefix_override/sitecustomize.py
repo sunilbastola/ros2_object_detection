ff9ac62594dbc53217d5018b263e12dbcc32efb3
Jo8903 <87169806+Jo8903@users.noreply.github.com>
2026-08-28 01:15:43 +1000

    Add comp8430_week11 classification_topic package, remove submission zip

---
+++ comp8430_week11/build/classification_topic/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/ubuntu/Desktop/48715638/comp8430_week11/install/classification_topic'
---
+++ comp8430_week11/src/classification_topic/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'classification_topic'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='ubuntu@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+	        "classification_subscriber = classification_topic.classification_sub:main",
+	        "capture_images = classification_topic.capture_images:main"
+        ],
+    },
+)
